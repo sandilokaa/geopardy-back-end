26621cc09b2ee307c6c820022b2ed3b4a95b39f2
sandilokaa <sandyloka790@gmail.com>
2023-11-21 00:45:25 +0700

    docs: add CityDetails table

---
+++ migrations/20231120174221-create-city-details.js
@@ -0,0 +1,58 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('CityDetails', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      adminId: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'Admins',
+          key: 'id'
+        }
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'Cities',
+          key: 'id'
+        }
+      },
+      riskLevelId: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'RiskLevels',
+          key: 'id'
+        }
+      },
+      latitude: {
+        type: Sequelize.STRING
+      },
+      longitude: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      picture: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('CityDetails');
+  }
+};
---
+++ models/citydetails.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class CityDetails extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+
+      CityDetails.belongsTo(models.Admins, {
+        foreignKey: 'adminId'
+      });
+
+      CityDetails.belongsTo(models.Cities, {
+        foreignKey: 'cityId'
+      });
+
+      CityDetails.belongsTo(models.RiskLevels, {
+        foreignKey: 'riskLevelId'
+      });
+      
+    }
+  }
+  CityDetails.init({
+    adminId: DataTypes.INTEGER,
+    cityId: DataTypes.INTEGER,
+    riskLevelId: DataTypes.INTEGER,
+    latitude: DataTypes.STRING,
+    longitude: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    picture: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'CityDetails',
+  });
+  return CityDetails;
+};
